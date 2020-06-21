2211274607178a05330e491506d79530445cc335
maazbin <maaz.mstqm@gmail.com>
2020-06-21 16:25:17 +0500

    migration of listings,realtors

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
+# Listing model
 
-# Create your models here.
+class Listing(models.Model):
+    # DO_NOTHING : No cascade delete
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    discription = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedroom = models.IntegerField()
+    bathroom = models.DecimalField(max_digits=2,decimal_places=1)
+    garage = models.IntegerField(default=0)
+    # Area
+    sqft = models.IntegerField()
+    lot_size = models.IntegerField()   
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    list_date=models.DateTimeField(default=datetime.now, blank=True)
+    is_published = models.BooleanField(default=True)
+    # main field
+    def __str__(self):
+        return self.title
