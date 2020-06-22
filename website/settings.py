bd8574edcccd6972d82de07f81b5a0d045df62b4
maazbin <maaz.mstqm@gmail.com>
2020-06-22 12:26:41 +0500

    admin set up

--- website/settings.py
+++ website/settings.py
@@ -116,3 +116,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'website/static')
 ]
+# Media Folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
