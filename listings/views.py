b935b1f5c9e19bf8facffc701b8b95107cfbae5c
maazbin <maaz.mstqm@gmail.com>
2020-06-23 13:11:49 +0500

    data fatched from db

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator
 
-# Create your views here.
+from .models import Listing
+
+
 
 def index(request):
-    return render(request, 'listings/index_listings.html')
+    # SQL query with ORM 
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)
+    
+    paginator = Paginator(listings, 2)
+    page_number = request.GET.get('page')
+    # page object 
+    paged_listings = paginator.get_page(page_number)
+    
+    # dictionary for templete
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/index_listings.html',context)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+# single listing
+def listing(request,listing_id):
+    listing = get_object_or_404(Listing,pk = listing_id)
+
+    context = {
+        'listing' : listing
+    }
+
+    return render(request, 'listings/listing.html',context)
 
 def search(request):
     return render(request, 'listings/search.html')
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email','hire_date')
+    list_display = ('id','name','email','hire_date','is_mvp')
     list_display_links = ('id','name')
     search_fields = ('name',)
+    list_editable = ('is_mvp',)
     list_per_page = 20
 
 admin.site.register(Realtor,RealtorAdmin)
